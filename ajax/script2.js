2ee52ae1ce6b2b6dc12fc1f0014ce654f6aa5ac9
paoloceravolo <paolo.ceravolo@unimi.it>
2021-04-29 13:01:32 +0200

    Update Ajax

--- ajax/script2.js
+++ ajax/script2.js
@@ -5,14 +5,18 @@ reqHeader.append("Access-Control-Allow-Origin", "*");
 
 let initObject = {
     method: 'GET',
-    mode: 'cors'
+    mode: 'cors',
     headers: reqHeader
 };
 
 let myRequest = new Request('https://www.dati.lombardia.it/resource/nmru-kdry.json', initObject);
 
 fetch(myRequest)
-.then()
-.then()
-.catch();
-
+    .then(function(response){return response.json()})
+    .then(function(data){
+        document.getElementById('content')
+        .innerHTML += data[0].denominazione;
+    })
+    .catch(function(err){
+        console.log("Errore!!!", err)
+    });
